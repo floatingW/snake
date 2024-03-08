8c15092e69aef493ad22d5e00f14ec19b276356c
fuwei <fuwei.chn@outlook.com>
2024-03-08 16:09:57 +0800

    main loop

--- main.cpp
+++ main.cpp
@@ -4,13 +4,14 @@
 #include <iostream> // cout
 #include <ostream>
 #include <queue>
-#include <unistd.h> // fd
 #include <sys/ioctl.h> // ioctl()
 #include <utility> // make_pair()
 #include <vector>
 #include <array>
 #include <cassert>
 #include <random>
+#include <chrono>
+#include <thread>
 
 namespace detail
 {
@@ -31,6 +32,11 @@ namespace detail
         struct winsize w;
         ioctl(STDOUT_FILENO, TIOCGWINSZ, &w);
         return std::make_pair(w.ws_row, w.ws_col);
+    }
+
+    auto TickToMilliSeconds(unsigned tick_per_sec)
+    {
+        return static_cast<unsigned>(1.0 / tick_per_sec * 1000);
     }
 }
 
@@ -66,6 +72,43 @@ enum class Direction
     Down,
     Left,
     Right
+};
+
+struct Position
+{
+    Position() = default;
+    Position(size_t x, size_t y) :
+        x(x), y(y)
+    {
+    }
+    size_t x{};
+    size_t y{};
+
+    Position Step(Direction dir) const
+    {
+        Position p(*this);
+        switch (dir)
+        {
+        case Direction::Down:
+            --p.y;
+            break;
+        case Direction::Up:
+            ++p.y;
+            break;
+        case Direction::Left:
+            --p.x;
+            break;
+        case Direction::Right:
+            ++p.x;
+            break;
+        default:
+        {
+            assert(false);
+        }
+        }
+
+        return p;
+    }
 };
 
 template<typename T>
@@ -153,8 +196,12 @@ namespace detail
         return Draw(pos.x, pos.y, v);
     }
 
-}
+    auto IsOutOfBoundary(Position pos, size_t width, size_t height)
+    {
+        return pos.x < 0 || pos.x > width || pos.y < 0 || pos.y > height;
+    }
 
+}
 
 int main()
 {
@@ -181,6 +228,46 @@ int main()
     snake.push_front(Position(initial_pos_x, initial_pos_y));
 
     // initial direction
+    Direction dir{ Direction::Left };
+
+    while (true)
+    {
+        Position current_pos = snake.front();
+        Position next_pos = current_pos.Step(dir);
+
+        if (detail::IsOutOfBoundary(next_pos, console_size_x, console_size_y))
+        {
+            std::cout << "End" << std::endl;
+            break;
+        }
+
+        // step forward
+        snake.push_front(next_pos);
+
+        if (map.Get(next_pos) == Tile::Food)
+        {
+            map.Set(next_pos, Tile::Snake);
+            detail::Draw(next_pos, Tile::Snake);
+
+            // next food
+            auto random_pos = map.RandomTile(Tile::Open);
+            map.Set(random_pos, Tile::Food);
+            detail::Draw(random_pos, Tile::Food);
+        }
+        else
+        {
+            // An open tile
+
+            map.Set(next_pos, Tile::Snake);
+            detail::Draw(next_pos, Tile::Snake);
+            map.Set(snake.back(), Tile::Open);
+            detail::Draw(snake.back(), Tile::Open);
+
+            snake.pop_back();
+        }
+
+        std::this_thread::sleep_for(std::chrono::milliseconds(detail::TickToMilliSeconds(2)));
+    }
 
     return 0;
 }
